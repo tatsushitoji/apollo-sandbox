d7770676d8bedbc6aca03dd804283aded3be7776
tatsushitoji <28854043+tatsushitoji@users.noreply.github.com>
2018-12-20 09:16:42 +0900

    test: add correctly mutation test

--- src/Todo/TodoForm.test.tsx
+++ src/Todo/TodoForm.test.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
-import { Mutation } from 'react-apollo';
 import { MockedProvider } from 'react-apollo/test-utils';
 import { mount } from 'enzyme';
-
 import { CREATE_TODO } from '../graphql';
-import { TodoForm } from '.';
+import { TodoForm, TodoFormComponent } from '.';
 
 test('should render without error', () => {
   const mockSubmit = jest.fn();
@@ -37,7 +35,7 @@ test('should render button is enable', () => {
   expect(wrapper.find('.ant-btn').prop('disabled')).toBeFalsy();
 });
 
-test('should called onSubmit and mutate method', async () => {
+test('should called onSubmit', async () => {
   const mockSubmit = jest.fn();
   const mocks = {
     request: {
@@ -69,5 +67,37 @@ test('should called onSubmit and mutate method', async () => {
   );
   await new Promise(resolve => setTimeout(resolve, 10)); // wait for response
   wrapper.update(); // apply re-render
-  expect(mockSubmit).toHaveBeenCalledTimes(1); // TODO: correctly
+  expect(mockSubmit).toHaveBeenCalledTimes(1);
+});
+
+test('should called mutate', () => {
+  const spy = jest.fn();
+  const mockSubmit = (form: any, mutate: any) => (e: React.FormEvent) => {
+    e.preventDefault();
+    mutate({
+      variables: {
+        createTodoInput: {
+          id: 'id1',
+          title: 'todo1',
+          completed: false,
+          createdAt: `1544594653658`,
+        },
+      },
+    }).then(spy('success mutation'));
+  };
+  const mockForm = {
+    getFieldDecorator: () => (node: React.ReactNode) => node,
+    getFieldValue: () => 'todo1',
+  };
+  const mockMutate = (_: any) => Promise.resolve();
+  const wrapper = mount(
+    <TodoFormComponent
+      form={mockForm as any}
+      mutate={mockMutate}
+      onSubmit={mockSubmit}
+    />,
+  );
+  wrapper.find('.ant-input').simulate('change', { target: { value: 'todo1' } });
+  wrapper.find('.ant-btn').simulate('submit');
+  expect(spy).toHaveBeenCalledWith('success mutation');
 });
