dbf60de83f2e3db8d39ce114a30c508669717a7e
tatsushitoji <28854043+tatsushitoji@users.noreply.github.com>
2018-12-21 09:17:35 +0900

    feat: enabled code generation from static file

--- apollo.config.js
+++ apollo.config.js
@@ -2,6 +2,7 @@ module.exports = {
   client: {
     includes: ['src/graphql/*.ts'],
     service: {
+      localSchemaFile: "schema.json",
       name: "apollo-sandbox",
       url: "http://localhost:4000/graphql",
     }
